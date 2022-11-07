de60590c7139d1b4ee73cafed93cc048ecf984dc
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2022-11-07 15:26:59 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [time, setTime] = useState(new Date())
+
+    const updateTimestamp = () => {
+        setTime(new Date())
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <Text>Kasvit</Text>
+            <Button
+                onPress={updateTimestamp}
+                title="Water"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
